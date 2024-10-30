4ffa9883d0c6a2b6012efaaff85eeea217a37585
Biki Dey <deybiki721@gmail.com>
2024-10-30 10:39:42 +0530

    Portfolio built using React and Framer Motion

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+import Technologies from "./Technologies";
+import Experience from "./Experience";
+import Projects from "./Projects";
+import Contact from "./Contact";
+
+export { Navbar, Hero, About, Technologies, Experience, Projects, Contact };
